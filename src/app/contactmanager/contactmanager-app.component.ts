7264cd905024d219b148d027c1e08c7c10c2a77c
Jason Fedler <Jason.Fedler@gmail.com>
2023-08-07 19:59:47 -0400

    initial commit for angular-material styling course

---
+++ src/app/contactmanager/contactmanager-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactmanager-app',
+  template: `
+    <app-side-nav></app-side-nav>
+  `,
+  styles: [
+  ]
+})
+export class ContactmanagerAppComponent {
+
+}
