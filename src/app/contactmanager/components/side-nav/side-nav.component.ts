dfaed8066af14c1ae105977b561e0d5e69f29be1
Jason Fedler <Jason.Fedler@gmail.com>
2023-08-09 21:35:34 -0400

    adds routing capabilities and hides sidebar when on small screen after navigation;

--- src/app/contactmanager/components/side-nav/side-nav.component.ts
+++ src/app/contactmanager/components/side-nav/side-nav.component.ts
@@ -1,8 +1,10 @@
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { Observable } from 'rxjs';
 import { User } from '../../models/user';
+import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
 
 const SMALL_WIDTH_BREAKPOINT = 720;
 
@@ -12,28 +14,30 @@ const SMALL_WIDTH_BREAKPOINT = 720;
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
-  public isScreenSmall: boolean;
+  public isScreenSmall: boolean = true;
 
-  users: Observable<User[]> | undefined;
+  users: Observable<User[]>;
 
   constructor(private breakpointObserver: BreakpointObserver,
-    private userService: UserService) { 
-    this.isScreenSmall = true;
-  }
+    private userService: UserService,
+    private router: Router) { }
+
+  @ViewChild(MatSidenav) sidenav: MatSidenav;
 
   ngOnInit(): void {
     this.breakpointObserver
       .observe([ `(max-width: ${SMALL_WIDTH_BREAKPOINT}px)` ])
         .subscribe((state: BreakpointState) => {
           this.isScreenSmall = state.matches;
-          console.log("triggered")
         });
 
     this.users = this.userService.users;
     this.userService.loadAll();
 
-    this.users.subscribe(data => {
-      console.log(data);
+    this.router.events.subscribe(() => {
+      if (this.isScreenSmall) {
+        this.sidenav.close();
+      }
     });
   }
 }
--- src/app/contactmanager/services/user.service.ts
+++ src/app/contactmanager/services/user.service.ts
@@ -3,18 +3,16 @@ import { User } from '../models/user';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UserService {
 
   private _users: BehaviorSubject<User[]>;
 
   private dataStore: {
-    users: User[]; 
+    users: User[];
   }
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
     this.dataStore = { users: [] };
     this._users = new BehaviorSubject<User[]>([]);
   }
@@ -23,17 +21,29 @@ export class UserService {
     return this._users.asObservable();
   }
 
+  addUser(user: User): Promise<User> {
+    return new Promise((resolver, reject) => {
+      user.id = this.dataStore.users.length + 1;
+      this.dataStore.users.push(user);
+      this._users.next(Object.assign({}, this.dataStore).users);
+      resolver(user);
+    })
+  }
+
+  userById(id: number) {
+    return this.dataStore.users.find(x => x.id == id);
+  }
+
   loadAll() {
-    const usersUrl = "https://angular-material-api.azurewebsites.net/users";
+    const usersUrl = 'https://angular-material-api.azurewebsites.net/users'
 
     return this.http.get<User[]>(usersUrl)
       .subscribe(data => {
         this.dataStore.users = data;
         this._users.next(Object.assign({}, this.dataStore).users);
-      },
-      error => {
-        console.log("Failed to fetch users via HTTP. Retrieving from local");
-        // TODO: read from api-call.json and return results
+      }, error => {
+        console.log("Failed to fetch users")
       });
   }
+
 }
